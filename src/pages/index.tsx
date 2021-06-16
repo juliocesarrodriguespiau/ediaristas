fbed6571f0e3229dd4cddf457ae25939610b9a7f
juliocesarrodriguespiau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-06-16 10:21:21 -0300

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
-//import Head from 'next/head';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
-import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import Head from "next/head";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle 
-        title={'Conheça os Profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionais de sua localidade'} 
+      <PageTitle
+        title={"Conheça os Profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais de sua localidade"
+        }
       />
 
       <UserInformation />
